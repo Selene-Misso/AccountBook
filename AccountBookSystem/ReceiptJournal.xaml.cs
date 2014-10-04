17f7c7aa91a9f922269e89e474c16e612a419583
Selene-Misso <mishiro.24@gmail.com>
2014-10-04 11:34:35 +0900

    データベースからテストデータを表示させたかったが，上手く行かなかった．WPFの新機能で別のやり方がありそうなので，そっちを勉強する．

--- AccountBookSystem/ReceiptJournal.xaml.cs
+++ AccountBookSystem/ReceiptJournal.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -20,9 +23,15 @@ namespace AccountBookSystem
 	/// </summary>
 	public partial class ReceiptJournal : Window
 	{
+		private string path = @"C:\Users\Owner\Documents\Visual Studio 2013\Projects\AccountBookSystem\AccountBookSystem\bin\Debug\settings\database.accdb";
+		private string strSql = "SELECT * FROM TestTable";
+		private OleDbConnection oleCon;
+		private OleDbCommand oleComd;
+
 		public ReceiptJournal()
 		{
 			InitializeComponent();
+			LoadDB();
 		}
 
 		private void Button_Click(object sender, RoutedEventArgs e)
@@ -52,6 +61,46 @@ namespace AccountBookSystem
 			this.warekiNum.Text = jcal.GetYear(today).ToString();
 		}
 
+		private void LoadDB()
+		{
+			oleCon = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";");
+			oleComd = new OleDbCommand(strSql, oleCon);
+			DataTable dt = new DataTable("dt_TestTable");
+			
+			// 列の定義
+			dt.Columns.Add("dt_ID", typeof(int));
+			dt.Columns.Add("dt_氏名", typeof(string));
+			dt.Columns.Add("dt_読み", typeof(string));
 
+			try
+			{
+				oleCon.Open();
+				using (OleDbDataAdapter adapter = new OleDbDataAdapter())
+				{
+					adapter.SelectCommand = oleComd;
+
+					// マッピング
+					adapter.TableMappings.Add("TestTable","dt_TestTable");
+					adapter.TableMappings["TestTable"].ColumnMappings.Add("ID", "dt_ID");
+					adapter.TableMappings["TestTable"].ColumnMappings.Add("氏名", "dt_氏名");
+					adapter.TableMappings["TestTable"].ColumnMappings.Add("読み", "dt_読み");
+
+					// DataSetオブジェクトにない列orテーブルは無視
+					//adapter.MissingMappingAction = MissingMappingAction.Ignore;
+
+					viewDataGrid.DataContext = dt;
+
+					adapter.Fill(dt);
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			finally
+			{
+				oleCon.Close();
+			}
+		}
 	}
 }
